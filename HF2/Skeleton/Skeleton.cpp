4397b745e1713fa8223d64eef6ef671cedcebc83
erikboros <boros.erik@gmail.com>
2019-04-26 13:04:58 +0200

    almost fading mirrors

--- HF2/Skeleton/Skeleton.cpp
+++ HF2/Skeleton/Skeleton.cpp
@@ -39,10 +39,18 @@ struct Material {
 	vec3 F0;
 	Material(vec3 _kd, vec3 _ks, float _shininess, bool _rough) : ka(_kd * M_PI), kd(_kd), ks(_ks), rough(_rough) {
 		shininess = _shininess; 
-		F0 = vec3(0.9, 0.85, 0.8);
+		vec3 one(1, 1, 1);
+		vec3 n(0.17, 0.35, 1.5);
+		vec3 kappa(3.1, 2.7, 1.9);
+		vec3 F0(0, 0, 0);
+		F0.x = ((n.x - one.x)*(n.x - one.x) + kappa.x * kappa.x) /
+			((n.x + one.x)*(n.x + one.x) + kappa.x * kappa.x);
+		F0.y = ((n.y - one.y)*(n.y - one.y) + kappa.y * kappa.y) /
+			((n.y + one.y)*(n.y + one.y) + kappa.y * kappa.y);
+		F0.z = ((n.z - one.z)*(n.z - one.z) + kappa.z * kappa.z) /
+			((n.z + one.z)*(n.z + one.z) + kappa.z * kappa.z);
 	}
 };
-
 
 struct Hit {
 	float t;
@@ -62,6 +70,7 @@ protected:
 public:
 	virtual Hit intersect(const Ray& ray) = 0;
 };
+
 
 struct Sphere : public Intersectable {
 	vec3 center;
@@ -92,6 +101,7 @@ struct Sphere : public Intersectable {
 	}
 };
 
+/*
 struct Side : public Intersectable {
 	std::vector<vec3> r;
 	Side(Material* _material) {
@@ -121,6 +131,7 @@ struct Side : public Intersectable {
 		return hit;
 	}
 };
+*/
 
 struct Wall : public Intersectable {
 	std::vector<vec3> r;
@@ -130,15 +141,13 @@ struct Wall : public Intersectable {
 		r.push_back(b);
 		r.push_back(c);
 		r.push_back(d);
-
 	}
 	Hit intersect(const Ray& ray) {
 		Hit hit;
-		hit.t = 0;
+		hit.t = -1;
 		vec3 normal = normalize(cross((r[1] - r[0]), (r[2] - r[0])));
 		float t = dot((r[0] - ray.start), normal) / dot(ray.dir, normal);
 		vec3 p = ray.start + ray.dir * t;
-
 		if (   dot(cross(r[1] - r[0], p - r[0]), normal) > 0
 			&& dot(cross(r[0] - r[2], p - r[2]), normal) > 0
 			&& dot(cross(r[2] - r[3], p - r[3]), normal) > 0
@@ -158,13 +167,21 @@ struct PolyTube{
 	Material *material;
 	int n;
 	float d;
+	PolyTube() {
+		n = 3;
+		d = 0.4;
+	}
+	void setMaterial(Material* _material) {
+		material = _material;
+	}
 	PolyTube(Material* _material) {
 		material = _material;
-		n = 5;
+		n = 3;
 		d = 0.4;
 	}
 	void addSide() {
 		n++;
+		//printf("polytube: sides=%d\n", n);
 	}
 	void putSides(std::vector<Intersectable *> &o) {
 		for (int i = 0; i < n-1; i++) {
@@ -291,6 +308,7 @@ struct Light {
 float rnd() { return (float)rand() / RAND_MAX; }
 
 const float epsilon = 0.0001f;
+PolyTube mirror;
 
 class Scene {
 	std::vector<Intersectable *> objects;
@@ -300,48 +318,38 @@ class Scene {
 public:
 	void build() {
 		vec3 eye = vec3(0, 0, 2), vup = vec3(0, 1, 0), lookat = vec3(0, 0, 0);
-		float fov = 80 * M_PI / 180;
+		float fov = 75 * M_PI / 180;
 		camera.set(eye, lookat, vup, fov);
 
-		La = vec3(0.4f, 0.4f, 0.4f);
-		vec3 lightDirection(0, 0, 1), Le(2, 2, 2);
+		La = vec3(1.0f, 0.5f, 0.5f);
+		vec3 lightDirection(0, 0, 1), 
+			Le(1.1, 1.2, 1.5);
 		lights.push_back(new Light(lightDirection, Le));
 
-		//lightDirection = vec3(-1, 1, 1);
-		//lights.push_back(new Light(lightDirection, Le));
+		vec3 kd(0.5f, 0.5f, 1.0f); 
+		vec3 ks(0.5, 0.5, 0.5);
+		Material * material = new Material(kd, ks, 20, true);
+		objects.push_back(new Sphere(vec3(0, 0, 0), 0.15, material));
 
-		vec3 kd(0.3f, 0.2f, 0.1f); vec3 ks(2, 2, 2);
-		Material * material = new Material(kd, ks, 40, true);
-		/*
-		for (int i = 0; i < 25; i++) {
-			kd = vec3(rnd() / 3, rnd() / 3, rnd() / 3);
-			ks= vec3(2, 2, 2);
-			material = new Material(kd, ks, 40, true);
-			
-			objects.push_back(new Sphere(vec3(rnd() - 0.5, rnd() - 0.5, rnd() - 0.5), rnd() * 0.1, material));
-		}
-		*/
-		objects.push_back(new Sphere(vec3(0, 0, 0), 0.2, material));
-
-		kd=vec3(0.3f, 0.2f, 0.1f); ks=vec3(2, 2, 2);
-		Material * smaterial = new Material(kd, ks, 50, false);
-		objects.push_back(new Sphere(vec3(0, 0, 0), 0.2, smaterial));
-
-
-		PolyTube mirror(smaterial);
+		Material * smaterial = new Material(kd, ks, 0, false);
+		mirror.setMaterial(smaterial);
 		mirror.putSides(objects);
 
-		/*
-		vec3 a(-0.5, 0, 0);
-		vec3 b(0.5, 0, 0);
-		vec3 c(-0.5, -0.1, 1);
-		vec3 d(0.5, -0.1, 1);
-		objects.push_back(new Wall(a, c, b, d, material));*/
+		kd= vec3(1, 1, 1);
+		Material * white = new Material(kd, ks, 1, true);
+		objects.push_back(new Wall(vec3(-1, 1, 0), vec3(-1, -1, 0), vec3(1, 1,0), vec3(1, -1, 0), white));
+
+
+	}
+
+	void clear() {
+		objects.clear();
+		lights.clear();
 	}
 
 	void render(std::vector<vec4>& image) {
 		for (int Y = 0; Y < windowHeight; Y++) {
-#pragma omp parallel for
+			#pragma omp parallel for
 			for (int X = 0; X < windowWidth; X++) {
 				vec3 color = trace(camera.getRay(X, Y));
 				image[Y * windowWidth + X] = vec4(color.x, color.y, color.z, 1);
@@ -364,58 +372,45 @@ public:
 		return false;
 	}
 
-	vec3 Fresnel(vec3 F0, float cosTheta) {
-		return F0 + (vec3(1, 1, 1) - F0) * pow(cosTheta, 5);
+	vec3 Fresnel(vec3 inDir, vec3 normal, vec3 F0){//vec3 F0, float cosTheta) {
+		//return F0 + (vec3(1, 1, 1) - F0) * pow(cosTheta, 5);
+		float cosa = -dot(inDir, normal);
+		vec3 one(1, 1, 1);
+		vec3 ret = F0 + (one - F0)*pow(1 - cosa, 5);
+		//printf("fresnel: (%f,%f,%f)\n", ret.x, ret.y, ret.z); //(0.8,0.9, 0.8)
+		return ret;
 	}
 
 	vec3 reflect(vec3 I, vec3 N) {
 		return I - N * 2.0f * dot(N, I);
 	}
 
-	vec3 trace(Ray ray, int maxdepth=5) {
-		/*
-		Hit hit = firstIntersect(ray);
-		if (hit.t < 0) return La;
-		vec3 outRadiance = hit.material->ka * La;
-		for (Light * light : lights) {
-			Ray shadowRay(hit.position + hit.normal * epsilon, light->direction);
-			float cosTheta = dot(hit.normal, light->direction);
-			if (cosTheta > 0 && !shadowIntersect(shadowRay)) {	// shadow computation
-				outRadiance = outRadiance + light->Le * hit.material->kd * cosTheta;
-				vec3 halfway = normalize(-ray.dir + light->direction);
-				float cosDelta = dot(hit.normal, halfway);
-				if (cosDelta > 0) outRadiance = outRadiance + light->Le * hit.material->ks * powf(cosDelta, hit.material->shininess);
-			}
-		}
-		return outRadiance;
-		*/		
+	vec3 trace(Ray ray, int maxdepth=10) {	
 		vec3 weight = vec3(1, 1, 1);
 		vec3 outRadiance = vec3(0, 0, 0);
+		Light *light = lights[0];
 
 		for (int d = 0; d < maxdepth; d++) {
 			Hit hit = firstIntersect(ray);
-			if (hit.t < 0) return La;
+			if (hit.t < 0) return weight * La;
 			if (hit.material->rough) {
-				vec3 outRadiance = hit.material->ka * La;
-				for (Light * light : lights) {
-					Ray shadowRay(hit.position + hit.normal * epsilon, light->direction);
-					float cosTheta = dot(hit.normal, light->direction);
-					if (cosTheta > 0 && !shadowIntersect(shadowRay)) {	// shadow computation
-						outRadiance = outRadiance + light->Le * hit.material->kd * cosTheta;
-						vec3 halfway = normalize(-ray.dir + light->direction);
-						float cosDelta = dot(hit.normal, halfway);
-						if (cosDelta > 0) outRadiance = outRadiance + light->Le * hit.material->ks * powf(cosDelta, hit.material->shininess);
-					}
+				outRadiance = outRadiance + weight* hit.material->ka * La;
+				Ray shadowRay(hit.position + hit.normal * epsilon, light->direction);
+				float cosTheta = dot(hit.normal, light->direction);
+				if (cosTheta > 0 && !shadowIntersect(shadowRay)) {	// shadow computation
+					outRadiance = outRadiance + weight * light->Le * hit.material->kd * cosTheta;
+					vec3 halfway = normalize(-ray.dir + light->direction);
+					float cosDelta = dot(hit.normal, halfway);
+					if (cosDelta > 0) outRadiance = outRadiance + weight * light->Le * hit.material->ks * powf(cosDelta, hit.material->shininess);
 				}
-				return outRadiance;
 			}
-			if (!hit.material->rough) {
-				weight = Fresnel(hit.material->F0, dot(-ray.dir, hit.normal)) * weight;
+			if (!hit.material->rough) { //reflective
+				weight = Fresnel(ray.dir, hit.normal, hit.material->F0) * weight;//Fresnel(hit.material->F0, dot(-ray.dir, hit.normal)) * weight;
 				ray.start = hit.position + hit.normal * epsilon;
 				ray.dir = reflect(ray.dir, hit.normal);
-			}
-			else return outRadiance;
+			} else return outRadiance;
 		}
+
 		
 	}
 };
@@ -465,10 +460,14 @@ public:
 		// vertex coordinates: vbo0 -> Attrib Array 0 -> vertexPosition of the vertex shader
 		glBindBuffer(GL_ARRAY_BUFFER, vbo); // make it active, it is an array
 		float vertexCoords[] = { -1, -1,  1, -1,  1, 1,  -1, 1 };	// two triangles forming a quad
-		glBufferData(GL_ARRAY_BUFFER, sizeof(vertexCoords), vertexCoords, GL_STATIC_DRAW);	   // copy to that part of the memory which is not modified 
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertexCoords), vertexCoords, GL_DYNAMIC_DRAW);	   // copy to that part of the memory which is not modified 
 		glEnableVertexAttribArray(0);
 		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, NULL);     // stride and offset: it is tightly packed
 
+		pTexture = new Texture(windowWidth, windowHeight, image);
+	}
+
+	void setImage(std::vector<vec4>& image) {
 		pTexture = new Texture(windowWidth, windowHeight, image);
 	}
 
@@ -482,13 +481,13 @@ public:
 };
 
 FullScreenTexturedQuad fullScreenTexturedQuad;
+std::vector<vec4> image(windowWidth * windowHeight);
 
 // Initialization, create an OpenGL context
 void onInitialization() {
 	glViewport(0, 0, windowWidth, windowHeight);
 	scene.build();
 
-	std::vector<vec4> image(windowWidth * windowHeight);
 	long timeStart = glutGet(GLUT_ELAPSED_TIME);
 	scene.render(image);
 	long timeEnd = glutGet(GLUT_ELAPSED_TIME);
@@ -501,12 +500,30 @@ void onInitialization() {
 
 // Window has become invalid: Redraw
 void onDisplay() {
+	//printf("ondisp\n");
 	fullScreenTexturedQuad.Draw();
 	glutSwapBuffers();									// exchange the two buffers
 }
 
 // Key of ASCII code pressed
 void onKeyboard(unsigned char key, int pX, int pY) {
+	if (key == 'a') {
+		//printf("space\n");
+		mirror.addSide();
+		//printf("side added\n");
+		scene.clear();
+		scene.build();
+		//printf("scene built\n");
+		//image = std::vector<vec4>(windowWidth * windowHeight);
+
+		long timeStart = glutGet(GLUT_ELAPSED_TIME);
+		scene.render(image);
+		long timeEnd = glutGet(GLUT_ELAPSED_TIME);
+		//printf("Rendering time: %d milliseconds\n", (timeEnd - timeStart));
+		fullScreenTexturedQuad.setImage(image);
+		//printf("image set\n");
+		glutPostRedisplay();
+	}
 }
 
 // Key of ASCII code released
